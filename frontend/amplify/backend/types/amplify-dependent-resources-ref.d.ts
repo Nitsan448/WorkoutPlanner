6bf20bbd585032ddbfdfb9c5bf73fe28083f2bae
Nitsan448 <Nitsan447@gmail.com>
2023-02-03 23:27:23 +0200

    Preperations for deployment with aws

--- frontend/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "amplifyDatasource": {
-      "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
-    }
-  }
-}
